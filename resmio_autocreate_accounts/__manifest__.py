42c113cb8f1d07dd72db55d3cdc046199018a384
Sascha Pfeiffer <sascha.pfeiffer@resmio.com>
2021-09-02 11:59:35 +0200

    Added resmio_autocreate_accounts

---
+++ resmio_autocreate_accounts/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Resmio autocreate accounts",
+    'description': """
+    Automatically create payable and receivable accounts on first invoice
+    """,
+    'category': '',
+    'version': '1.0',
+
+    'depends': ['sale'],
+    'data': [
+        'data/sequences.xml',
+    ],
+    'demo': [
+    ],
+}
