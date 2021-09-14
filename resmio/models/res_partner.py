7f07e22713a206c3e7ed4e3f887a1323a74dd3b6
Sascha Pfeiffer <sascha.pfeiffer@resmio.com>
2021-09-14 13:29:56 +0200

    added some more fields

--- resmio/models/res_partner.py
+++ resmio/models/res_partner.py
@@ -6,6 +6,11 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     facility_id = fields.Char('Facility ID')
+    enabled = fields.Boolean('Enabled')
+    verified = fields.Boolean('Verified')
+    in_partner_network = fields.Boolean('In partner network')
+    bitburger_customer_number = fields.Char('Bitburger customer number')
+    valid_payment_information = fields.Boolean('Valid payment information')
     cmp_source = fields.Char('CMP Source')
     cmp_medium = fields.Char('CMP Medium')
     cmp_name = fields.Char('CMP Name')
